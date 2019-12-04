04e2530d042cf827be77fef234fa635e6578a574
Alexander Panko <god@panki.ru>
2019-12-04 12:47:55 +0100

    Add constants

---
+++ Constants.cs
@@ -0,0 +1,28 @@
+namespace ProtestGoClient
+{
+    namespace Constants
+    {
+        // Any network connection error
+        public static class ProtestMode
+        {
+            public static readonly uint LEAFLET = 1;
+            public static readonly uint PROTEST = 2;
+        }
+
+        public static class ProtestTypes
+        {
+            public static readonly unit LIBERAL = 1;
+            public static readonly unit CONSERVATIVE = 2;
+            public static readonly uint GREENPEACE = 3;
+            public static readonly uint MONSTRATION = 4;
+        }
+
+        public static class AvatarTypes
+        {
+            public static readonly unit INITIAL = 1;
+            public static readonly uint ONSELL = 2;
+        }
+
+    }
+}
+
