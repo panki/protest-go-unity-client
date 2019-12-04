fdda39a978a5eb9d989cfb673dc739109bef043c
Alexander Panko <god@panki.ru>
2019-12-04 12:56:00 +0100

    Fix constants

--- Constants.cs
+++ Constants.cs
@@ -11,15 +11,15 @@ namespace ProtestGoClient
 
         public static class ProtestTypes
         {
-            public static readonly unit LIBERAL = 1;
-            public static readonly unit CONSERVATIVE = 2;
+            public static readonly uint LIBERAL = 1;
+            public static readonly uint CONSERVATIVE = 2;
             public static readonly uint GREENPEACE = 3;
             public static readonly uint MONSTRATION = 4;
         }
 
         public static class AvatarTypes
         {
-            public static readonly unit INITIAL = 1;
+            public static readonly uint INITIAL = 1;
             public static readonly uint ONSELL = 2;
         }
 
