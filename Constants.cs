6c9fc801dba5752fe4dfcf8cd5b7ab5ed4e66e3d
Alexander Panko <god@panki.ru>
2020-06-05 13:24:42 -0500

    chore(constant): add event types

--- Constants.cs
+++ Constants.cs
@@ -3,21 +3,29 @@ namespace ProtestGoClient.Constants
     // Any network connection error
     public static class ProtestMode
     {
-        public static readonly uint LEAFLET = 1;
-        public static readonly uint PROTEST = 2;
+        public static readonly uint Leaflet = 1;
+        public static readonly uint Protest = 2;
     }
 
     public static class AvatarTypes
     {
-        public static readonly uint INITIAL = 1;
-        public static readonly uint ONSELL = 2;
+        public static readonly uint Initial = 1;
+        public static readonly uint OnSell = 2;
     }
 
     public static class Currency
     {
-        public static readonly string REAL = "real";
-        public static readonly string LIBERO = "libero";
-        public static readonly string ORDERO = "ordero";
+        public static readonly string Real = "real";
+        public static readonly string Libero = "libero";
+        public static readonly string Ordero = "ordero";
+    }
+
+    public static class Events
+    {
+        public static readonly string ProtestCreated = "protest.created";
+        public static readonly string ProtestJoined = "protest.joined";
+        public static readonly string ProtestLeaved = "protest.leaved";
+        public static readonly string ProtestFinished = "protest.finished";
     }
 
 }
